836b530ca6984b186dc67acbbce5a264a4fbcc85
Driinho <pedro.nascimento.henrique2003@gmail.com>
2022-10-22 23:15:54 -0300

    Adicionando serviços aos projetos

--- src/components/pages/Project/Project.js
+++ src/components/pages/Project/Project.js
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from 'uuid'
+
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
@@ -5,6 +7,7 @@ import Loading from '../../layout/Loading/Loading'
 import Container from '../../layout/Container/Container'
 import ProjectForm from '../../project/ProjectForm'
 import Message from '../../layout/Message/Message'
+import ServiceForm from '../Service/ServiceForm'
 
 import styles from './Project.module.css'
 
@@ -56,6 +59,45 @@ function Project() {
                 setShowProjectForm(false)
                 setMessage('Projeto Atualizado')
                 setType('success')
+            })
+            .catch(err => console.log(err))
+    }
+
+    function createService(project) {
+        setMessage('')
+
+        //last service
+        const lastService = project.services[project.services.length - 1]
+
+        lastService.id = uuidv4()
+
+        const lastServiceCost = lastService.cost
+
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+        // maximum value validation
+        if (newCost > parseFloat(project.budget)) {
+            setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+            setType('error')
+            project.services.pop()
+            return false
+        }
+
+        // add service cost to project total cost
+        project.cost = newCost
+
+        // update project
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                //exibir os serviços
+                console.log(data)
             })
             .catch(err => console.log(err))
     }
@@ -119,7 +161,11 @@ function Project() {
                             </button>
                             <div className={styles.project_info}>
                                 {showServiceForm && (
-                                    <div>Formulário do serviço</div>
+                                    <ServiceForm
+                                        handleSubmit={createService}
+                                        btnText="Adicionar Serviço"
+                                        projectData={project}
+                                    />
                                 )}
                             </div>
                         </div>
